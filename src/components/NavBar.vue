67ac3d39552dfe214f9d1732b1d7d5e13a413098
DaveyClonts <David.Clonts@eagles.oc.edu>
2025-09-24 14:54:00 -0500

    WIP

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+
+</script>
+
+<template>
+    <div class="side-bar">
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .side-bar {
+        display: flex;
+        direction: column;
+        align-items: left;
+    }
+</style>
