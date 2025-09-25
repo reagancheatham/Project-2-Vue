5c1dbc1a68991337523f34464c66ea5a41733d42
DaveyClonts <David.Clonts@eagles.oc.edu>
2025-09-24 21:45:11 -0500

    wip

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,12 +2,13 @@
 import { ref } from 'vue';
 
 
+
 </script>
 
 <template>
     <div class="side-bar">
-        <ul>
-            <li>1</li>
+        <ul class="filters">
+            <li class="list-item">1</li>
             <li>2</li>
             <li>3</li>
         </ul>
@@ -15,9 +16,13 @@ import { ref } from 'vue';
 </template>
 
 <style scoped>
-    .side-bar {
-        display: flex;
-        direction: column;
-        align-items: left;
+    .filters {
+        list-style-type: none;
+        width: 20%;
+        background-color: #949492;
+        position: fixed;
+        height: 100%;
+        top: 0;
+        left: 0;
     }
 </style>
