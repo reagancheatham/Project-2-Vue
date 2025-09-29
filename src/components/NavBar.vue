3954bdd7e3244566a30de474eb9117063ff123dd
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-09-29 09:43:45 -0500

    Dlc add navbar component2 (#11)
    * WIP
    
    * wip

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+
+
+</script>
+
+<template>
+    <div class="side-bar">
+        <ul class="filters">
+            <li class="list-item">1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .filters {
+        list-style-type: none;
+        width: 20%;
+        background-color: #949492;
+        position: fixed;
+        height: 100%;
+        top: 0;
+        left: 0;
+    }
+</style>
