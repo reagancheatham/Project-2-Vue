da11f790d3df53927d77409e79d0d26dbe6f2026
DaveyClonts <David.Clonts@eagles.oc.edu>
2025-09-29 09:44:08 -0500

    welp oppsie

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,28 +1,52 @@
 <script setup>
-import { ref } from 'vue';
-
-
-
+import AccordionButton from "./AccordionButton.vue";
 </script>
 
 <template>
-    <div class="side-bar">
-        <ul class="filters">
-            <li class="list-item">1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </div>
+  <ul class="side-bar">
+    <li class="title">CourseDB</li>
+    <li class="item">
+      <accordion-button title="Department"> 
+        <div class="button-content">
+            test
+        </div>
+      </accordion-button>
+    </li>
+    <li class="item">
+      <accordion-button title="Level"> </accordion-button>
+    </li>
+    <li class="item">
+      <accordion-button title="Hour"> </accordion-button>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
-    .filters {
-        list-style-type: none;
-        width: 20%;
-        background-color: #949492;
-        position: fixed;
-        height: 100%;
-        top: 0;
-        left: 0;
-    }
+.side-bar {
+  color: #000000;
+  border-right: 1px solid #000000;
+  list-style-type: none;
+  width: 18%;
+  position: fixed;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+.title {
+  padding: 4px;
+  font-size: 28px;
+  font-weight: 600;
+  text-align: left;
+}
+
+.item {
+  padding: 4px;
+  text-align: left;
+}
+
+.button-content {
+    text-align: center;
+    /* TODO: this will be for however we do filters */
+}
 </style>
