ce49f4fa5f850049b868f47edd84f32025128345
Maximus253 <122314662+Maximus253@users.noreply.github.com>
2025-10-01 09:00:44 -0500

    Mj add import functionality (#17)
    * Set up button and parsing
    
    do some more stuff
    
    * Display is now working lets go
    
    * feat: add import prompts component
    
    * feat: DB tie
    
    ties import to database

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,48 +1,50 @@
 <script setup>
 import AccordionButton from "./AccordionButton.vue";
+import ImportButton from "./ImportButton.vue";
 </script>
 
 <template>
-  <ul class="side-bar">
-    <li class="title">CourseDB</li>
-    <li class="item">
-      <accordion-button title="Department"> 
-        <div class="button-content">
-            test
-        </div>
-      </accordion-button>
-    </li>
-    <li class="item">
-      <accordion-button title="Level"> </accordion-button>
-    </li>
-    <li class="item">
-      <accordion-button title="Hour"> </accordion-button>
-    </li>
-  </ul>
+    <ul class="side-bar">
+        <li class="title">CourseDB</li>
+        <li class="item">
+            <accordion-button title="Department">
+                <div class="button-content">test</div>
+            </accordion-button>
+        </li>
+        <li class="item">
+            <accordion-button title="Level"> </accordion-button>
+        </li>
+        <li class="item">
+            <accordion-button title="Hour"> </accordion-button>
+        </li>
+        <li class="item">
+            <import-button />
+        </li>
+    </ul>
 </template>
 
 <style scoped>
 .side-bar {
-  color: #000000;
-  border-right: 1px solid #000000;
-  list-style-type: none;
-  width: 18%;
-  position: fixed;
-  height: 100%;
-  top: 0;
-  left: 0;
+    color: #000000;
+    border-right: 1px solid #000000;
+    list-style-type: none;
+    width: 18%;
+    position: fixed;
+    height: 100%;
+    top: 0;
+    left: 0;
 }
 
 .title {
-  padding: 4px;
-  font-size: 28px;
-  font-weight: 600;
-  text-align: left;
+    padding: 4px;
+    font-size: 28px;
+    font-weight: 600;
+    text-align: left;
 }
 
 .item {
-  padding: 4px;
-  text-align: left;
+    padding: 4px;
+    text-align: left;
 }
 
 .button-content {
