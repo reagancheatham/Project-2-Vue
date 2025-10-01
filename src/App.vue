a4e3250e249b8be1db8f556c60d3d8e830f4dc4e
James-Wiley <james.wiley@live.com>
2025-10-01 13:27:24 -0500

    finish list items

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <script setup>
+import { ref } from "vue";
 import NavBar from "./components/NavBar.vue";
 import courseService from "./services/courseServices";
 import CourseDisplay from "./components/CourseDisplay.vue";
 
-let courses = [];
+let courses = ref([]);
 courseService.getAll()
 .then(response => {
   console.log(JSON.stringify(response));
-  courses = response;
-  console.log(courses);
+  courses.value = response;
+  console.log(courses.value);
 })
 .catch(err => console.log(err));
 </script>
 
 <template>
     <NavBar />
-    <div class="grid-container">
+    <div class="grid-container class-list">
       <CourseDisplay
         v-for="course in courses"
         :key="course.courseNumber"
@@ -24,3 +25,12 @@ courseService.getAll()
       />
     </div>
 </template>
+
+<style>
+.class-list {
+  margin-left: 18%;
+  padding: 10px;
+  display: grid;
+  gap: 10px;
+}
+</style>
--- src/components/CourseDisplay.vue
+++ src/components/CourseDisplay.vue
@@ -1,14 +1,36 @@
 <script setup>
 import { ref } from "vue";
+import ViewPopup from "./ViewPopup.vue";
+
 const props = defineProps(["course"]);
+const showViewPopup = ref(false);
+
 console.log(props.course);
 
-
+const handleClick = () => {
+    showViewPopup.value = true;
+};
 </script>
 <template>
-    <div class="class-card">
-        <h2>{{ props.course.dept }}-{{ props.course.courseNum }}</h2>
+    <div class="class-card" @click="handleClick">
+        <h2>{{ props.course.courseNumber }}</h2>
         <h1>{{ props.course.name }}</h1>
         <p>Level: {{ props.course.level }} Hour: {{ props.course.hours }}</p>
     </div>
+    
+    <ViewPopup
+        v-model:show="showViewPopup"
+        :course="props.course"
+    />
 </template>
+
+<style scoped>
+.class-card {
+  border: 1px solid #000000;
+  border-radius: 8px;
+  padding: 16px;
+  background-color: #f9f9f9;
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+}
+</style>
