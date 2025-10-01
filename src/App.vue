d2e7c581ca439ace83646faa9290114bc11bbc99
James-Wiley <james.wiley@live.com>
2025-10-01 11:02:59 -0500

    add course display

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
 import courseService from "./services/courseServices";
+import CourseDisplay from "./components/CourseDisplay.vue";
 
+let courses = [];
 courseService.getAll()
-.then(response => console.log(JSON.stringify(response)))
+.then(response => {
+  console.log(JSON.stringify(response));
+  courses = response;
+  console.log(courses);
+})
 .catch(err => console.log(err));
 </script>
 
 <template>
     <NavBar />
+    <div class="grid-container">
+      <CourseDisplay
+        v-for="course in courses"
+        :key="course.courseNumber"
+        :course="course"
+        @deletedCourse="getCourses()"
+      />
+    </div>
 </template>
---
+++ src/components/CourseDisplay.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps(["course"]);
+console.log(props.course);
+
+
+</script>
+<template>
+    <div class="class-card">
+        <h2>{{ props.course.dept }}-{{ props.course.courseNum }}</h2>
+        <h1>{{ props.course.name }}</h1>
+        <p>Level: {{ props.course.level }} Hour: {{ props.course.hours }}</p>
+    </div>
+</template>
